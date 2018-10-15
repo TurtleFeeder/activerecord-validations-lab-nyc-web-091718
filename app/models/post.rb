f1becd57e1bd3f3be5012beb1c31f366ecb4dd25
TurtleFeeder <t.choong@yahoo.com>
2018-10-15 15:40:25 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait_worthy
+
+  def is_clickbait_worthy
+    worthyTitles = ["Won't Believe", "Secret", "Top [number]",  "Guess"]
+    if title == nil
+      errors.add(:title, "title missing")
+    else
+      if worthyTitles.any? {|string| title.include?(string)} == false
+        errors.add(:title, "not clickbait-y enough")
+      end
+    end
+  end #end is_clickbait_worthy method
+
 end
